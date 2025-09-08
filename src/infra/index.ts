1e285d4afb8d074164cc2edeee70a551b474b062
allankevinscain <meuemail44allan@gmail.com>
2025-09-08 17:12:56 -0300

    chore: export

--- src/infra/index.ts
+++ src/infra/index.ts
@@ -4,3 +4,7 @@ server.listen('8080', () => {
   console.log(`Server started at ${new Date().toISOString()}`);
   console.log(`Server running on port 8080`);
 });
+
+export default (req: any, res: any) => {
+  server(req, res);
+};
