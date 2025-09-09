0b01fd6c68ea42cd02b9bddf9cd19f678810f836
allankevinscain <meuemail44allan@gmail.com>
2025-09-09 14:12:04 -0300

    chore: adjusts to register builder manager record

--- src/schemas/user.schema.ts
+++ src/schemas/user.schema.ts
@@ -3,7 +3,7 @@ import { z } from 'zod';
 export const UserSchema = z.object({
   email: z.string().email(),
   password: z.string(),
-  loginType: z.enum(['admin', 'manager']),
+  loginType: z.enum(['admin', 'manager', 'builder-manager']),
   name: z.string(),
   city: z.string(),
   createdAt: z.string().optional(),
